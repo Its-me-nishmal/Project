89b39d6ea44d2f7847c33e6807975a0c09472477
its-me-nishmal <twalaba123@gmail.com>
2023-11-08 14:42:03 +0530

    connection settled

--- .config/admin_auth.js
+++ .config/admin_auth.js
@@ -1,6 +1,9 @@
 const Admin = require('../model/Admin');
+const mongodb = require('./.config/dbconnect')
+
 const auth = async (req, res, next) => {
   try {
+    await mongodb()
     const token = req.cookies.admin_token;
     const admin = await Admin.findOne({ tokens: token });
     if (!admin) return res.redirect('/admin/login')
