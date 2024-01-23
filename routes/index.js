b02e2b563cb079add2252a9581d5ea3662bdb0a8
its-me-nishmal <twalaba123@gmail.com>
2024-01-23 15:09:42 +0530

    landing on

--- routes/index.js
+++ routes/index.js
@@ -64,17 +64,91 @@ const fetchVideos = async (id) => {
 
 router.get('/', async function(req, res, next) {
   let link = ""
+  const instructors = [
+    {
+      name: "Professor Celestia Starlight",
+      role: "Cosmic Education Guide",
+      image: "img/team-1.jpg",
+      social: [
+        { icon: "fab fa-facebook-f", link: "#" },
+        { icon: "fab fa-twitter", link: "#" },
+        { icon: "fab fa-instagram", link: "#" }
+      ]
+    },
+    {
+      name: "Dr. Quantum Lumina",
+      role: "Chief Learning Navigator",
+      image: "img/team-2.jpg",
+      social: [
+        { icon: "fab fa-facebook-f", link: "#" },
+        { icon: "fab fa-twitter", link: "#" },
+        { icon: "fab fa-instagram", link: "#" }
+      ]
+    },
+    {
+      name: "Captain Nebula Beacon",
+      role: "Educational Voyager-in-Chief",
+      image: "img/team-3.jpg",
+      social: [
+        { icon: "fab fa-facebook-f", link: "#" },
+        { icon: "fab fa-twitter", link: "#" },
+        { icon: "fab fa-instagram", link: "#" }
+      ]
+    },
+    {
+      name: "Madam Solaris Insight",
+      role: "Enlightenment Engineer",
+      image: "img/team-4.jpg",
+      social: [
+        { icon: "fab fa-facebook-f", link: "#" },
+        { icon: "fab fa-twitter", link: "#" },
+        { icon: "fab fa-instagram", link: "#" }
+      ]
+    }
+  ];
+  // testimonials.js
+
+const testimonials = [
+  {
+    name: "Sophia Rodriguez",
+    designation: "Learning Pioneers",
+    image: "img/testimonial-1.jpg",
+    text: "Dive into a cosmos of knowledge at Cosmos Mastery School. Our courses spark curiosity, leading students on a journey of discovery that goes beyond textbooks."
+  },
+  {
+    name: "Elijah Thompson",
+    designation: "Stellar Scholars",
+    image: "img/testimonial-2.jpg",
+    text: "Navigate the virtual cosmos with engaging online classes. Skilled instructors create an immersive learning experience, fostering critical thinking skills for future success."
+  },
+  {
+    name: "Ava Patel",
+    designation: "Knowledge Voyagers",
+    image: "img/testimonial-3.jpg",
+    text: "Earn stellar international certificates as you progress. These not only validate expertise but also open doors to exciting opportunities, empowering you to shine on a global scale."
+  },
+  {
+    name: "Caleb Morgan",
+    designation: "Cosmic Achievers",
+    image: "img/testimonial-4.jpg",
+    text: "Beyond courses, we empower students for lifelong learning. Graduates leave not just with knowledge but also the skills and confidence to navigate the real-world cosmos."
+  }
+];
+
+module.exports = testimonials;
+
   try {
     link = await Banner.find();
     const teachers = await Teacher.find();
     const  students = await Students.find()
   const videos = await fetchVideos(link[0].playlist);
-  console.log(videos.length)
-  res.render('index',{videos,teachers,students})
+  
+  res.render('index',{videos,teachers,students,instructors,testimonials})
 
-  } catch (e) {
-    console.error(e)
-  }
+} catch (e) {
+  console.error(e);
+  res.status(500).send('Internal Server Error');
+}
   
 });
 
@@ -82,4 +156,7 @@ router.get('/onesignal.js', (req, res) => {
   res.sendFile(__dirname + '/public/OneSignalSDKWorker.js');
 });
 
+router.get('/about',(re,rs)=>{
+  rs.render('about_home')
+})
 module.exports = router;
